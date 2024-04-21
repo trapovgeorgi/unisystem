3e710d1ae86f2920b8feda0b8213f2f0f0eaf807
Georgi Trapov <trapovgeorgi@gmail.com>
2024-04-21 20:50:25 +0300

    another one

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -7,21 +7,10 @@ use App\Models\Group;
 use App\Models\Specialty;
 use App\Models\User;
 use App\Services\NotificationService;
-use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 
 Route::post('/login', function (Request $request) {
